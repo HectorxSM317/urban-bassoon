f3d0ba8bf691c7f8cd32e00915be2e3e0131a66b
Hector Salgado <hectorsalgadomedina@gmail.com>
2022-12-13 12:41:28 -0300

    backend: database & environments

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+const environments = require('./environments');
+
+const sequelize = new Sequelize(`postgres://${environments.USER_DB}:${environments.PASSWORD_DB}@localhost:5433/${environments.DATABASE}`)
+
+
+const initSequelize = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+
+module.exports = { sequelize, initSequelize }
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const router = require('./routes') 
+const { initSequelize } = require('./config/database')
+
+
+app.use(express.json())
+initSequelize()
+
 
 app.use('/api', router)
 
-app.listen(4000 ,() => console.log(`Servidor corriendo en puerto 4000}`))
+app.listen(4000 ,() => console.log(`Servidor corriendo en puerto 4000`))
